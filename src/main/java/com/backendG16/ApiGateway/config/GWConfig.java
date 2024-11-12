bc6665147fdb501a44b54c912f27da1c1f594d6b
emmach02 <emmanuelricardochaile@hotmail.com>
2024-11-11 23:30:51 -0300

    primer commit del gateway

---
+++ src/main/java/com/backendG16/ApiGateway/config/GWConfig.java
@@ -0,0 +1,26 @@
+package com.backendG16.ApiGateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GWConfig {
+    @Bean
+    public RouteLocator configurarRutas(
+            RouteLocatorBuilder builder,
+            @Value("http://localhost:8080") String uriAlquiler)
+    {
+        return builder.routes()
+                .route(p -> p.path("/api/pruebas/**").uri(uriAlquiler))
+                .route(p -> p.path("/api/vehiculos/**").uri(uriAlquiler))
+                .route(p -> p.path("/api/empleados/**").uri(uriAlquiler))
+                .route(p -> p.path("/api/marcas/**").uri(uriAlquiler))
+                .route(p -> p.path("/api/modelos/**").uri(uriAlquiler))
+                .route(p -> p.path("/api/posiciones/**").uri(uriAlquiler))
+                .route(p -> p.path("/api/interesados/**").uri(uriAlquiler))
+                .build();
+    }
+}
---
+++ src/test/java/com/backendG16/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendG16.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
