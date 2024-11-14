bb5d9d73b989f3c199c50d134556f023d67595ce
emmach02 <emmanuelricardochaile@hotmail.com>
2024-11-14 00:56:19 -0300

    funcionando seguridad, salvo que le podemos pegar al micro directo

--- src/main/java/com/backendG16/ApiGateway/config/GWConfig.java
+++ src/main/java/com/backendG16/ApiGateway/config/GWConfig.java
@@ -5,15 +5,25 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class GWConfig {
 
-    @Value("${api.uri.alquiler}")
-    private String uriAlquiler;
+
 
     @Bean
-    public RouteLocator configurarRutas(RouteLocatorBuilder builder) {
+    public RouteLocator configurarRutas(RouteLocatorBuilder builder,
+                                        @Value("${api.uri.alquiler}") String uriAlquiler) {
         return builder.routes()
                 .route(p -> p.path("/api/vehiculos/**").uri(uriAlquiler))
                 .route(p -> p.path("/api/empleados/**").uri(uriAlquiler))
@@ -22,5 +32,44 @@ public class GWConfig {
                 .route(p -> p.path("/api/posiciones/**").uri(uriAlquiler))
                 .route(p -> p.path("/api/interesados/**").uri(uriAlquiler))
                 .build();
+    }
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+        http.authorizeExchange(exchanges -> exchanges
+                        // Esta ruta puede ser accedida por cualquiera, sin autorización
+                        .pathMatchers("/api/vehiculos/**")
+                        .permitAll()
+
+                        .pathMatchers("/api/empleados/**")
+                        .hasAnyRole("ADMIN", "EMPLEADO")
+
+                        // Cualquier otra petición...
+                        .anyExchange()
+                        .authenticated()
+
+                ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        // Se especifica el nombre del claim a analizar
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+        // Se agrega este prefijo en la conversión por una convención de Spring
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        // Se asocia el conversor de Authorities al Bean que convierte el token JWT a un objeto Authorization
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
+                new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter));
+        // También se puede cambiar el claim que corresponde al nombre que luego se utilizará en el objeto
+        // Authorization
+        // jwtAuthenticationConverter.setPrincipalClaimName("user_name");
+
+        return jwtAuthenticationConverter;
     }
 }
